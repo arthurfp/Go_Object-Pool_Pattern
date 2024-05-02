a4fc2e2a6dabe87f5970d10592ffb98408518638
Arthur Ferreira <fepi.arthur@gmail.com>
2024-05-02 16:27:18 -0300

    Implement dynamic configuration for connection pool settings

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"object-pool-go/pkg/pool"
 	"time"
 )
 
 func main() {
+	var maxOpenConns int
+	var timeout int
+
+	flag.IntVar(&maxOpenConns, "maxOpenConns", 5, "Maximum number of open connections in the pool")
+	flag.IntVar(&timeout, "timeout", 30, "Timeout in seconds for waiting to borrow a connection")
+	flag.Parse()
+
 	fmt.Println("Object Pool Pattern in Go")
 
-	connPool := pool.NewConnectionPool(2, 5*time.Second) // Pool size of 2 and timeout of 5 seconds
+	connPool := pool.NewConnectionPool(maxOpenConns, time.Duration(timeout)*time.Second)
 
-	// Borrow a connection
+	// Demonstrate borrowing a connection
 	conn, err := connPool.BorrowConnection()
 	if err != nil {
 		fmt.Println("Failed to borrow connection:", err)
 		return
 	}
 	fmt.Println("Borrowed Connection ID:", conn.ID)
-
-	// Simulate query
-	if err := conn.Execute("SELECT * FROM dummy_table"); err != nil {
-		fmt.Println("Failed to execute query:", err)
-	}
 
 	// Release the connection
 	connPool.ReleaseConnection(conn)
